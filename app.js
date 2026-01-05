35f473cbba7fb4b463abe01373b4f2e6a584354b
Ajay Ghadage <ajayghadage2005@gmail.com>
2026-01-05 23:38:19 +0530

    added files

--- app.js
+++ app.js
@@ -49,7 +49,9 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
+  touchAfter: 24 * 3600,
 });
+
 
 store.on("error", (err) => {
   console.log("ERROR IN MONGO SESSION STORE", err);
